5c33d21a84e2e32e588384a7acf3dec03fbf9b08
Deepak Mishra <dux.deepak@gmail.com>
2019-12-24 16:19:24 -0600

    added message composer

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -29,27 +29,37 @@ class _ChatScreenState extends State<ChatScreen> {
           )
         ],
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0))),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(30.0),
-            topLeft: Radius.circular(30.0),
-          ),
-          child: ListView.builder(
-            padding: EdgeInsets.only(top: 30.0),
-            reverse: true,
-            itemCount: messages.length,
-            itemBuilder: (BuildContext context, int index) {
-              final Message message = messages[index];
-              final bool isMe = message.sender.id == currentUser.id;
-              return _buildMessage(message, isMe);
-            },
-          ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0))),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  ),
+                  child: ListView.builder(
+                    padding: EdgeInsets.only(top: 30.0),
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
+                ),
+              ),
+            ),
+            _buildMessageComposer(),
+          ],
         ),
       ),
     );
@@ -82,7 +92,7 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  Container _buildMessageContent(bool isMe, Message message) {
+  Widget _buildMessageContent(bool isMe, Message message) {
     return Container(
       width: MediaQuery.of(context).size.width * 0.75,
       margin: isMe
@@ -119,6 +129,43 @@ class _ChatScreenState extends State<ChatScreen> {
                   fontSize: 16.0,
                   color: Colors.blueGrey,
                   fontWeight: FontWeight.w600)),
+        ],
+      ),
+    );
+  }
+
+  _buildMessageComposer(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 70.0,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.photo),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: (){},
+          ),
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.words,
+              onChanged: (value ) {
+                setState(() {
+
+                });
+              },
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send a message...'
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            iconSize: 25.0,
+            color: Theme.of(context).primaryColor,
+            onPressed: (){},
+          )
         ],
       ),
     );
