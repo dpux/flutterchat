e2e6967edc57a52c25557dfed527efa570f55b33
Deepak Mishra <dux.deepak@gmail.com>
2019-12-23 20:09:57 -0600

    nav bar and category selector

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:chat/widgets/category_selector.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: (){},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 28.0
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: (){},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          CategorySelector()
+        ],
+      )
+    );
+  }
+}
